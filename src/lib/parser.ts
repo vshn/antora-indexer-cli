1ef8a4f4f39994b90be88a1ef4f2f8c9cd4fdf49
Adrian Kosmaczewski <adrian@vshn.ch>
2020-01-14 11:20:12 +0100

    Added possibility to parse other modules than ROOT

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -12,12 +12,29 @@ import * as git from 'isomorphic-git'
 git.plugins.set('fs', fs)
 
 /**
+ * Removes HTML comments from the string passed as parameter.
+ * Source:
+ * https://stackoverflow.com/a/5654032
+ * @param source The string to remove comments from
+ */
+function removeHtmlComments(source: string): string {
+	const regexp = new RegExp(
+    '<!--[\\s\\S]*?(?:-->)?'
+    + '<!---+>?'  // A comment with no body
+    + '|<!(?![dD][oO][cC][tT][yY][pP][eE]|\\[CDATA\\[)[^>]*>?'
+    + '|<[?][^>]*>?',  // A pseudo-comment
+		'g')
+	return source.replace(regexp, '')
+}
+
+/**
  * Returns the main title of the Asciidoc document passed as parameter.
  * This is usually the first paragraph, with a `=` prefix.
  * @param asciidoc An asciidoc document
  */
 function extractTitle(asciidoc: any): string {
-	return asciidoc.getDocumentTitle({ partition: false })
+	const title = asciidoc.getDocumentTitle({ partition: false })
+	return removeHtmlComments(title).trim()
 }
 
 /**
@@ -25,9 +42,13 @@ function extractTitle(asciidoc: any): string {
  * @param componentName The name of the Antora component
  * @param version The version of the Antora component
  * @param filename The filename of the AsciiDoc document
+ * @param moduleName The module name (not used if "ROOT")
  */
-function buildHref(componentName: string, version: string, filename: string): string {
-	return path.join('/', componentName, version, filename.replace('adoc', 'html'))
+function buildHref(componentName: string, moduleName: string, version: string, filename: string): string {
+	if (moduleName === 'ROOT') {
+		return path.join('/', componentName, version, filename.replace('adoc', 'html'))
+	}
+	return path.join('/', componentName, moduleName, version, filename.replace('adoc', 'html'))
 }
 
 /**
@@ -39,7 +60,7 @@ function extractExcerpt(text: string): string {
 	const sanitized: string = text.replace(/\n\n/g, ' ').replace(/\n/g, '')
 	const tokens: string[] = sanitized.split(' ').slice(0, 20)
 	const excerpt: string = tokens.join(' ') + '…'
-	return excerpt
+	return excerpt.trim()
 }
 
 /**
@@ -52,7 +73,31 @@ function extractText(asciidoc: any): string {
 	const htmlDoc: jsdom.JSDOM = new JSDOM(htmlString)
 	const body: HTMLElement = htmlDoc.window.document.body
 	const textContent: string = body.textContent || ''
-	return textContent
+	return textContent.replace('partial$meta-info-table.adoc', '')
+}
+
+/**
+ * Asynchronous-friendly for-each loop.
+ *
+ * Adapted from
+ * https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+ * @param array Array of items being iterated upon
+ * @param callback The function to call for each item
+ */
+async function asyncForEach(array: any, callback: any) {
+	for (let index = 0; index < array.length; index++) {
+		await callback(array[index], index, array)
+	}
+}
+
+/**
+ * Returns the names of subdirectories in the specified source location.
+ * @param source An array of strings with the name of subdirectory names
+ */
+function getDirectories(source: fs.PathLike): string[] {
+	return fs.readdirSync(source, { withFileTypes: true })
+		.filter(dirent => dirent.isDirectory())
+		.map(dirent => dirent.name)
 }
 
 /**
@@ -72,46 +117,46 @@ export function parseAntoraFile(startPath: string): ParsedFileEntry[] {
 	const antora: any = yaml.safeLoad(fs.readFileSync(antoraPath, 'utf8'))
 	const componentName: string = antora.name
 	let version: string = antora.version
-	const startPage: string = antora.start_page
-	const startPageComponents: string[] = startPage.split(':')
-	const moduleName: string = startPageComponents[0]
 
 	// For versionless components, Antora uses the 'master' value
 	if (version == 'master') {
 		version = ''
 	}
 
-	// The pages of an Antora module are always inside a particular path
-	const pagesPath: string = path.resolve(path.join(startPath, 'modules', moduleName, 'pages'))
-	const files: string[] = fs.readdirSync(pagesPath)
-
-	// Read the contents of each file and build the index array
+	// Get all module names
+	const moduleNames: string[] = getDirectories(path.resolve(path.join(startPath, 'modules')))
 	const lunrIndex: ParsedFileEntry[] = []
-	files.forEach(function (filename: string, index: number) {
-		const filePath: string = path.join(pagesPath, filename)
-		const asciidoc: any = asciidoctor.loadFile(filePath)
-		const href: string = buildHref(componentName, version, filename)
-		const name: string = extractTitle(asciidoc)
-		const text: string = extractText(asciidoc)
-		const excerpt: string = extractExcerpt(text)
-		const obj: ParsedFileEntry = {
-			name: name,
-			text: text,
-			href: href,
-			excerpt: excerpt,
-		}
-		lunrIndex.push(obj)
+
+	moduleNames.forEach(moduleName => {
+		// The pages of an Antora module are always inside a particular path
+		const pagesPath: string = path.resolve(path.join(startPath, 'modules', moduleName, 'pages'))
+		const files: string[] = fs.readdirSync(pagesPath)
+
+		// Read the contents of each file and build the index array
+		files.forEach(function (filename: string, index: number) {
+			const filePath: string = path.join(pagesPath, filename)
+			const asciidoc: any = asciidoctor.loadFile(filePath)
+			const href: string = buildHref(componentName, moduleName, version, filename)
+			const name: string = extractTitle(asciidoc)
+			const text: string = extractText(asciidoc)
+			const excerpt: string = extractExcerpt(text)
+			const obj: ParsedFileEntry = {
+				name: name,
+				text: text,
+				href: href,
+				excerpt: excerpt,
+			}
+			lunrIndex.push(obj)
+		})
 	})
 	return lunrIndex
 }
 
-// https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
-async function asyncForEach(array: any, callback: any) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
+/**
+ * Parses an Antora playbook.yml file, iterating through all the
+ * content sources, and indexes the contents of each.
+ * @param startPath The folder where the playbook.yml file is located
+ */
 export async function parsePlaybookFile(startPath: string): Promise<ParsedFileEntry[]> {
 	const playbookPath: string = path.resolve(path.join(startPath, 'playbook.yml'))
 
@@ -126,12 +171,12 @@ export async function parsePlaybookFile(startPath: string): Promise<ParsedFileEn
 	const playbook: any = yaml.safeLoad(fs.readFileSync(playbookPath, 'utf8'))
 	const sources: object[] = playbook.content.sources
 
-	// For each entry in the playbook, clone the repo and index it
+	// For each entry in the playbook, git clone the repo and index it
 	await asyncForEach(sources, async function (source: any, index: number) {
 		const url = source.url
 		const branch = source.branches
 
-		const clonesPath = path.join('.', '.repos', `d${Math.floor(Math.random()*1000001)}`)
+		const clonesPath = path.join('.', '.repos', `d${Math.floor(Math.random() * 1000001)}`)
 		fs.mkdirSync(clonesPath, { recursive: true })
 
 		// Clone the repo
